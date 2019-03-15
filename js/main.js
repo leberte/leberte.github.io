bbaf00c3fa19efc0a0222293ee26eaab4554c899
xucaihou <2511903043@qq.com>
2019-03-15 15:07:21 +0800

    Site updated: 2019-03-15 15:07:20

--- js/main.js
+++ js/main.js
@@ -1,1 +1,68 @@
-function initializeSite(){"use strict";!function(){function i(){var i=$(".sphere"),n=i.height(),e=$(window).height(),o=(e-n)/2;i.css({"margin-top":o+"px"});var t=$(".hero"),s=(e-t.height())/2;t.css({"margin-top":s+"px"})}$(document).ready(i),$(window).resize(i)}(),$("#scene").parallax()}$(window).load(function(){$(".preloader").fadeOut("slow")}),$(window).load(function(){initializeSite(),setTimeout(function(){window.scrollTo(0,0)},0)}),$("#countdown").countdown({data:"00:00:00",render:function(i){$(this.el).empty().append("<div>"+this.leadingZeros(i.hours,2)+" <span>hrs</span></div>").append("<div>"+this.leadingZeros(i.min,2)+" <span>min</span></div>").append("<div>"+this.leadingZeros(i.sec,2)+" <span>sec</span></div>")}});
+$(window).load(function(){
+     $('.preloader').fadeOut('slow');
+});
+
+
+/* =Main INIT Function
+-------------------------------------------------------------- */
+function initializeSite() {
+
+	"use strict";
+
+	//OUTLINE DIMENSION AND CENTER
+	(function() {
+	    function centerInit(){
+
+			var sphereContent = $('.sphere'),
+				sphereHeight = sphereContent.height(),
+				parentHeight = $(window).height(),
+				topMargin = (parentHeight - sphereHeight) / 2;
+
+			sphereContent.css({
+				"margin-top" : topMargin+"px"
+			});
+
+			var heroContent = $('.hero'),
+				heroHeight = heroContent.height(),
+				heroTopMargin = (parentHeight - heroHeight) / 2;
+
+			heroContent.css({
+				"margin-top" : heroTopMargin+"px"
+			});
+
+	    }
+
+	    $(document).ready(centerInit);
+		$(window).resize(centerInit);
+	})();
+
+	// Init effect 
+	$('#scene').parallax();
+
+};
+/* END ------------------------------------------------------- */
+
+/* =Document Ready Trigger
+-------------------------------------------------------------- */
+$(window).load(function(){
+
+	initializeSite();
+	(function() {
+		setTimeout(function(){window.scrollTo(0,0);},0);
+	})();
+
+});
+/* END ------------------------------------------------------- */
+
+
+$('#countdown').countdown({
+	data: "00:00:00",
+	render: function(data) {
+	  var el = $(this.el);
+	  el.empty()
+	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+	}
+});
